3e962b018367d04c47d915146c5e6e20adc11799
K.Adam White <kadamwhite@gmail.com>
2012-04-23 22:32:45 -0400

    Grunt-initialized plugin scaffolding

---
+++ src/jquery.rot13.js
@@ -0,0 +1,28 @@
+/*
+ * jquery.rot13
+ * https://github.com/kadamwhite/jquery.rot13
+ *
+ * Copyright (c) 2012 K.Adam White
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
